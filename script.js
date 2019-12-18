82581f8d59ca98a3d4add53b0697c0fc1d5b7e08
Dmboucher242 <58062008+Dmboucher242@users.noreply.github.com>
2019-12-17 22:25:07 -0700

    adding questions

--- script.js
+++ script.js
@@ -11,9 +11,31 @@ function startQuiz(){
 }
 
 function question1(){
-    
+
 }
 
 function selectAnswer(){
 
 }
+
+
+
+var questions = [
+    { 
+       question1: "which symbol represents an id?",
+       answer: [".", ",", "#", "!"],
+       
+       question2: "",
+       answers2: [""],
+       
+       question3: "",
+       answers3: [""],
+       
+       question4: "",
+       answers4: [""],
+       
+       question5: "",
+       answers5: [""]
+    }
+]
+
