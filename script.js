c652449bc49d3bde2fa4270d56d48e67d69d9779
Dmboucher242 <58062008+Dmboucher242@users.noreply.github.com>
2019-12-18 21:18:53 -0700

    added the answer button functions

--- script.js
+++ script.js
@@ -15,6 +15,16 @@
  }
  function showQuestion(question){
     questionElement.innerText = question.question; 
+    question.answers.forEach(answer => {
+        const button = document.createElement("button")
+        button.innerText = answer.text
+        button.classList.add("btn")
+        if (answer.correct){
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
  }
  
  function setNextQuestion(){
